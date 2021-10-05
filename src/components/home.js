c6710a8985dbfc97d0b66c415e94ab53c7d1167e
berenicehr <bere4758@gmail.com>
2021-10-05 10:58:51 -0500

    ok

--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,28 @@
+/* eslint-disable no-unused-expressions */
 import { onNavigate } from '../main.js';
-import {
-  logOut, getUser, postInFirestore, updatePost, db,
-} from '../firebaseAuth.js';
 import { allFunctions } from '../lib/validFunc.js';
+import {
+  logOut, getUser, postInFirestore, updatePost, deletePost, stateCheck, getTaskForEdit, editPost,
+} from '../firebaseAuth.js';
 
 export const home = () => {
   let userEmail = getUser();
   if (userEmail !== null) {
     userEmail = userEmail.email;
+  } else {
+    userEmail = '';
   }
+  // let editStatus = false;
   const homePage = document.createElement('div');
+
+  stateCheck(homePage);
+
   homePage.setAttribute('id', 'homePage');
   const htmlNodes = `<header id = "wallBanner" >
   <img id="logoWall" src="./imagenes/Imagen1.png">
   <h1 id="petFriendsWall">Pet Friends</h1>
   <img id="signOut" src= "./imagenes/exit.png"></header>
-  <h2 id= "welcomeMessage">Bienvenid@ ${userEmail} </h2>
+  <h2 id= "welcomeMessage">Bienvenid@ ${userEmail}</h2>
   <p id= "catchPost"></p>
   <div id="postContainer">
   <img id= "yellowDog" src="./imagenes/Güero.png">
@@ -25,65 +32,132 @@ export const home = () => {
   <div id="modal">
   <h3 id="close">x</h3>
   <textarea id="post" placeholder = "Cuéntanos sobre tu petFriend"></textarea>
-  <button id="share">Publicar</button>
+  <button id="share" class="send" >Publicar</button>
   </div>
   </div>
   <div id="posts"></div>
   `;
   homePage.innerHTML = htmlNodes;
-
+  const modal = homePage.querySelector('#backModal');
   const postDivPublish = homePage.querySelector('#posts');
 
-  updatePost((snapshot) => {
-    postDivPublish.innerHTML = '';
-    snapshot.forEach((doc) => {
-      const htmlPostsPublished = `<div id= "recentPostDiv">
-          <p id="userMail">${doc.data().user}</p>
-          <p id="recentPost">${doc.data().post}</p>
-          <div id= "divButtons"><button id= "edit">Editar</button>
-          <button id= "deletes"> Eliminar</button> 
-          <img id= "img" src="./imagenes/patitaGris.png">
-          </div>
-          </div>`;
-
-      postDivPublish.innerHTML += htmlPostsPublished;
-      /* postDivPublish.querySelector('#deletes').addEventListener('click', (e) => {
-        e.stopPropagation();
-        const id = e.target.parentElement.getAttribute('data-id');
-        db.collections('posts').doc(id).delete();
-      }); */
-      postDivPublish.querySelector('#deletes').addEventListener('click', (e) => {
-        e.stopPropagation();
-        const id = e.target.parentElement.getAttribute('data-id');
-        db.collections('posts').doc(id).delete();
-        console.log('hola');
-      });
-    });
-  });
-
-  const modal = homePage.querySelector('#backModal');
+  // Botón de cerrar sesión
   homePage.querySelector('#signOut').addEventListener('click', () => logOut(onNavigate));
 
+  // Botón para abrir el modal
   homePage.querySelector('#postInput').addEventListener('click', () => {
     modal.style.visibility = 'visible';
     homePage.querySelector('#post').value = '';
   });
 
+  // Es la x para cerrar el modal
   homePage.querySelector('#close').addEventListener('click', () => {
     modal.style.visibility = 'hidden';
   });
 
+  // Botón para publicar el post
   homePage.querySelector('#share').addEventListener('click', () => {
     modal.style.visibility = 'hidden';
-    // const catchPost = homePage.querySelector('#catchPost');
     const postPublish = homePage.querySelector('#post').value;
+    const date = new Date();
+    const postDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} a las ${date.getHours()}:${date.getMinutes()}`;
     if (allFunctions.validPost(postPublish) === false) {
       alert('No has publicado un post aún');
     } else {
-      postInFirestore(postPublish, userEmail);
+      postInFirestore(postPublish, userEmail, postDate);
     }
-
-    // const catchPost = homePage.querySelector('#catchPost');
   });
+
+  // Imprime los post ya existentes en pantalla
+  updatePost((snapshot) => {
+    postDivPublish.innerHTML = '';
+    snapshot.forEach((doc) => {
+      const comentId = doc.id;
+      const htmlPostsPublished = `<div id= "recentPostDiv" class= "completePost">
+          <p id="userMail" class="userMail" >${doc.data().user}:</p>
+          <p id="date">${doc.data().date}</p>
+          <p id="recentPost">${doc.data().post}</p>
+          ${userEmail === doc.data().user
+    ? `<div id= "divButtons">
+          <img id="img"  class= "like" src="./imagenes/patitaGris.png">
+          <button id= "edit" class= "btnEdit" data-id= ${comentId} >Editar</button>
+          <div class="editBackModal">
+          <div class="editModal">
+          <h3 class="editClose">x</h3>
+          <textarea class="editPost"></textarea>
+          <button id="share" class="save">Guardar</button>
+          </div>
+          </div>
+       <button id= "deletes" class="btndeletes" data-id= ${comentId} > Eliminar</button> 
+          <div class="deleteBackModal">
+          <div class="deleteModal" >
+          <h2 class= "confirmText">¿Estás segur@ que deseas eliminar este post? </h2>
+          <button class="si">Si</button>
+          <button class="no" >No</button>
+          </div>
+          </div>`
+    : '<img id="img"  class= "like" src="./imagenes/patitaGris.png">'}
+          </div>
+          </div>`;
+
+      postDivPublish.innerHTML += htmlPostsPublished;
+
+      // Función para manipular el like
+      const colorPaw = postDivPublish.querySelectorAll('.like');
+
+      colorPaw.forEach((postLike) => {
+        postLike.addEventListener('click', (e) => {
+          if (e.target.getAttribute('src') === './imagenes/patitaGris.png') {
+            postLike.setAttribute('src', './imagenes/patitaColor.png');
+          } else {
+            postLike.setAttribute('src', './imagenes/patitaGris.png');
+          }
+        });
+      });
+
+      // Botón para eliminar post
+      const deletebtn = postDivPublish.querySelectorAll('.btndeletes');
+
+      const deleteModal = postDivPublish.querySelector('.deleteBackModal');
+      deletebtn.forEach((btnDelete) => {
+        btnDelete.addEventListener('click', (f) => {
+          deleteModal.style.visibility = 'visible';
+          const confirmDelete = () => deletePost(f.target.dataset.id);
+          deleteModal.addEventListener('click', (e) => {
+            if (e.target.classList.contains('si')) {
+              confirmDelete();
+              deleteModal.style.visibility = 'hidden';
+            } else {
+              deleteModal.style.visibility = 'hidden';
+            }
+          });
+        });
+      });
+
+      // Botón para editar el post
+      const btnEdit = postDivPublish.querySelectorAll('.btnEdit');
+      const postEditModal = postDivPublish.querySelector('.editBackModal');
+      const editedPost = postDivPublish.querySelector('.editPost');
+      btnEdit.forEach((edtPost) => {
+        edtPost.addEventListener('click', async (event) => {
+          postEditModal.style.visibility = 'visible';
+          const docForEdit = await getTaskForEdit(event.target.dataset.id);
+          editedPost.value = docForEdit.data().post;
+          // console.log(docForEdit.data());
+          console.log(docForEdit.id);
+          postEditModal.addEventListener('click', (e) => {
+            if (e.target.classList.contains('save')) {
+              editPost(docForEdit.id, editedPost.value);
+              postEditModal.style.visibility = 'hidden';
+            }
+            if (e.target.classList.contains('editClose')) {
+              postEditModal.style.visibility = 'hidden';
+            }
+          });
+        });
+      });
+    });
+  });
+
   return homePage;
 };
